58de7b0051f833cae73d2f00083b4145b0c164b2
ShehrozAhmadDev <shehrozahmad970@gmail.com>
2024-03-23 11:43:38 +0500

    meta update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+      </head>
       <body className={`${inter.className}`}>
         <ReduxProvider>
           <ToastContainer theme="dark" />
